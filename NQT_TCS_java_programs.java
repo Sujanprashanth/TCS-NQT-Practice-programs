e2e470c1e8db40f42e98ee2e293ec950b37b6714
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2026-02-27 15:34:28 +0530

    Update NQT_TCS_java_programs.java

--- NQT_TCS_java_programs.java
+++ NQT_TCS_java_programs.java
@@ -366,3 +366,71 @@ Output
 ###***
 0
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Given an integer array Arr of size N the task is to find the count of elements whose value is greater than all of its prior elements.
+
+Note : 1st element of the array should be considered in the count of the result.
+
+For example,
+Arr[]={7,4,8,2,9}
+As 7 is the first element, it will consider in the result.
+8 and 9 are also the elements that are greater than all of its previous elements.
+Since total of  3 elements is present in the array that meets the condition.
+Hence the output = 3.
+Example 1:
+
+Input 
+5 -> Value of N, represents size of Arr
+7-> Value of Arr[0]
+4 -> Value of Arr[1]
+8-> Value of Arr[2]
+2-> Value of Arr[3]
+9-> Value of Arr[4]
+
+Output :
+3
+
+Example 2:
+5   -> Value of N, represents size of Arr
+3  -> Value of Arr[0]
+4 -> Value of Arr[1]
+5 -> Value of Arr[2]
+8 -> Value of Arr[3]
+9 -> Value of Arr[4]
+
+Output : 
+5
+
+Constraints
+
+1<=N<=20
+1<=Arr[i]<=10000
+
+Program:
+import java.util.*; 
+class Main { 
+  public static void main(String[] args) { 
+    Scanner sc = new Scanner(System.in); 
+    int s = sc.nextInt(); 
+    int[] arr = new int[s]; 
+    for(int i=0;i<arr.length;i++){ 
+      arr[i]=sc.nextInt(); 
+    }
+    int max = arr[0],count=0;
+    for(int i=1;i<arr.length;i++){ 
+      if(max<arr[i]){ 
+        max=arr[i];
+        count++;
+      }
+    }
+    System.out.println(count+1);
+  }
+}
+
+output:
+Sample input:
+6
+{5, 1, 6, 3, 7, 2}
+
+Sample output:
+3
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
