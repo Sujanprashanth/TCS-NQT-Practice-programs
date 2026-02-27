18d90b244d22a49f3dafd323f091520465b542cb
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2026-02-27 15:05:07 +0530

    Update NQT_TCS_java_programs.java

--- NQT_TCS_java_programs.java
+++ NQT_TCS_java_programs.java
@@ -326,7 +326,43 @@ Example Input
 Output
 TW =130 FW=70
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Program
+Problem Statement – Given a string S(input consisting) of ‘*’ and ‘#’. The length of the string is variable. The task is to find the minimum number of ‘*’ or ‘#’ to make it a valid string. The string is considered valid if the number of ‘*’ and ‘#’ are equal. The ‘*’ and ‘#’ can be at any position in the string.
+Note : The output will be a positive or negative integer based on number of ‘*’ and ‘#’ in the input string.
 
+(*>#): positive integer
+(#>*): negative integer
+(#=*): 0
+Example 1:
+Input 1:
+
+###***   -> Value of S
+Output :
+
+0   → number of * and # are equal
+  
+Program
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String w = sc.next();   
+        char[] arr = w.toCharArray();  
+        int t_count=0,h_count=0,s_count=0;
+        for(char arr1:arr){
+            if(arr1=='#'){
+                h_count++;
+            }
+            else{
+                s_count++;
+            }
+        }
+        t_count=s_count-h_count;
+        System.out.print(t_count);
+        
+    }
+}
   
 Output
+###***
+0
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
